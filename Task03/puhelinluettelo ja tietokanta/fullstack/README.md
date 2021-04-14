516a2125486916c9d67a6102c2c2191977f1ab00
aarnipavlidi <55801258+aarnipavlidi@users.noreply.github.com>
2021-04-14 20:02:27 +0300

    puhelinluettelo_tietokanta_fullstack

---
+++ Task03/puhelinluettelo ja tietokanta/fullstack/README.md
@@ -0,0 +1,10 @@
+## Heroku
+Following code has been tested and working on localhost and Heroku :)
+This directory (fullstack) exercise is following: "3.21 tietokantaa käyttävä versio internettiin"
+
+```
+https://arcane-savannah-24728.herokuapp.com/
+https://arcane-savannah-24728.herokuapp.com/info
+https://arcane-savannah-24728.herokuapp.com/api/persons
+
+```
---
+++ Task03/puhelinluettelo ja tietokanta/fullstack/models/persons.js
@@ -0,0 +1,47 @@
+// This exercise has been commented by Aarni Pavlidi, if you have any questions or suggestions with the code,
+// then please contact me by sending email at me@aarnipavlidi.fi <3
+
+const mongoose = require("mongoose") // Alustetaan muuttuja "mongoose", joka hyödyntää "mongoose" kirjastoa tämän sovelluksen osalta.
+// Alla oleva kirjasto otetaan käyttööön komennolla => "npm install --save mongoose-unique-validator".
+const uniqueValidator = require("mongoose-unique-validator") // Alustetaan muuttuja "uniqueValidator", joka hyödyntää "mongoose-unique-validator" kirjastoa tämän sovelluksen osalta.
+
+const database = process.env.MONGODB_URI // Alustetaan muuttuja "database" joka on yhtä kuin "process.env.MongoBD_URI". Arvo haetaan erikseen ".env" tiedostosta.
+
+console.log("Trying to connect to the following database ==>", database) // Tulostaa terminaaliin kyseisen tekstin näkyviin, jonka perään tulee vielä "database" muuttujan arvo.
+
+mongoose.connect(database, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true }) // Kyseisen funktion "mongoose.connect(...)" avulla voidaan yhdistää tietokantaan (MongoDB). Lisää infoa tästä @ https://mongoosejs.com/docs/connections.html
+ .then(result => { // Jos tietokantaan yhdistäminen onnistui, niin suoritetaan ".then()" funktio ja alla oleva asia.
+   console.log("Connected to the database successfully!") // Tulostaa terminaaliin kyseisen tekstin näkyviin.
+ })
+ .catch((error) => { // Mikäli tulee virheitä, kun yritetään yhdistää tietokantaan, niin suoritetaan ".catch()" funktio ja alla oleva asia.
+   console.log("There was problem while trying connect to the database:", error.message) // Tulostaa terminaaliin kyseisen tekstin näkyviin, jonka perään tulee vielä "error.message" muuttujan arvo.
+ })
+
+ const mongoFrame = new mongoose.Schema({ // Alustetaan muuttuja "mongoFrame", joka määrittelee miten me tallennetaan (missä rungossa) ne puhelinluettelon tietokantaan.
+   name: { // Tietokannan kokoelmasta löytyy "name" muuttuja => puhelinluettelo.persons.name
+     type: String, // Objektin "name" tyyppi on "String".
+     minlength: 3, // Kun käyttäjä haluaa lisätä uuden henkilön luetteloon, niin "name" objektin arvo täytyy olla vähintään kolme (3) merkkiä.
+     required: true, // Objekti "name" ei saa olla tyhjänä, muuten henkilöä ei viedä tietokantaan.
+     unique: true // Objektin "name" täytyy olla uniikki eli mikäli tietokannasta löytyy jo entuudestaan samanlainen arvo, niin henkilöä ei viedä tietokantaan.
+   },
+   number: { // Tietokonnan kokoelmasta löytyy "number" muuttuja => puhelinluettelo.persons.number
+     type: String, // Objektin "number" tyyppi on "String".
+     minlength: 8, // Kun käyttäjä haluaa lisätä uuden henkilön luetteloon, niin "number" objektin arvo täytyy olla vähintään kahdeksan (8) merkkiä.
+     required: true // Objekti "number" ei saa olla tyhjänä, muuten henkilöä ei viedä tietokantaan.
+   }
+ })
+
+ mongoFrame.plugin(uniqueValidator) // Jos tätä ei ole, niin kun tulee errori nii tulee "MongoError: E11000 duplicate key error collection..."
+
+ mongoFrame.set("toJSON", { // Tämän avulla voidaan määritellä missä "muodossa" sovellus näyttää JSON datan (tietokannsta tulevan datan eli MongoDB:stä) sivulla.
+   transform: (document, returnedObject) => { // "transform" funktion avulla voidaan tehdä muutoksia dataan, ennen palauttamista takaisin käyttäjälle.
+     returnedObject.id = returnedObject._id.toString() // Alustetaan muuttuja "returnedObject.id", joka on yhtä kuin "returnedObject._id.toString()".
+     delete returnedObject._id // Poistetaan ("piilotetaan" tietokannasta tuleva data) => "puhelinluettelo.persons" tietokannan sisällä oleva "_id" objekti.
+     delete returnedObject.__v // Poistetaan ("piilotetaan" tietokannasta tuleva data) => "puhelinluettelo.persons" tietokannan sisällä oleva "__v" objekti.
+   }
+ })
+
+ // Alustetaan "mongoose.model(...)" niminen funktio. Ota huomioon, jos "persons" olisi esim. "Aarni" niin data siirtyisi "aarnis" nimisen kokoelman alle.
+ // Tämä tarkoittaa siis sitä, että on poikkeuksia eli datan siirron yhteydessä MongoDB muuttaa kaikki kirjaimet pieneksi ja laittaa loppuun "s" kirjaimen.
+ // Alla oleva funktio saa siis arvot "persons" ja muuttujan "mongoFrame" arvon.
+ module.exports = mongoose.model("persons", mongoFrame) // Viedään (export) kyseinen funktio käytettäväksi sovelluksen "index.js" tiedostossa.
