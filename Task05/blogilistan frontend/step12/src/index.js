281068042ea0d89c39cec46f413c8968874f0344
aarnipavlidi <55801258+aarnipavlidi@users.noreply.github.com>
2021-05-11 18:01:51 +0300

    blogilistan_frontend_step12

---
+++ Task05/blogilistan frontend/step12/src/index.js
@@ -0,0 +1,9 @@
+// This exercise has been commented by Aarni Pavlidi, if you have any questions or suggestions with the code,
+// then please contact me by sending email at me@aarnipavlidi.fi <3
+
+import React from 'react' // Sovellus ottaa "react" nimisen kirjaston käyttöönsä.
+import ReactDOM from 'react-dom' // Sovellus ottaa "react-dom" nimisen kirjaston käyttöönsä.
+import App from './App' // Sovellus ottaa "App.js" tiedoston sisällön käyttöönsä.
+import './index.css' // Sovellus ottaa "index.css" (tyylitiedosto) tiedoston sisällön käyttöönsä.
+
+ReactDOM.render(<App />, document.getElementById('root'))
