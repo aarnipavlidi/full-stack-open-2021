2fcd7365c3ce5c2bce8c01120dad92718fd3086d
aarnipavlidi <55801258+aarnipavlidi@users.noreply.github.com>
2021-07-09 21:38:48 +0300

    9.8: Patientor backend, step1

---
+++ Task09/Patientor backend/step01/index.ts
@@ -0,0 +1,23 @@
+import express from 'express'; // Tuodaan "express" funktio sovelluksen käytettäväksi "express" nimisestä kirjastosta.
+const app = express(); // Alustetaan muuttuja "app", joka suorittaa kyseisen funktion.
+
+// Sovellus suorittaa kyseisen funktion, jotta voimme käsitellä esim. "POST" tyyppisiä
+// pyyntöjä ja sitä kautta käsitellä pyynnön kautta tulevaa dataa eli "request.body".
+app.use(express.json()); // Lisää funktiosta löytyy: https://expressjs.com/en/5x/api.html#express.json
+
+// Kun suoritetaan pyyntö osoitteeseen => "http://localhost:3003/ping",
+// niin sovellus suorittaa {...} sisällä olevat asiat ja palauttaa
+// käyttäjälle "Hello Full Stack!" tekstin näkyviin. Olemme myös
+// muuttaneet parametrin "req" => "_req", koska emme toistaiseksi käytä
+// kyseistä paramatria, koska muuten tulisi erroria!
+app.get('/ping', (_req, response) => {
+    console.log('Someone just pinged to this url!')
+    response.send('Hello Full Stack!'); // Lähettää kyseisen tekstin takaisin käyttäjälle näkyviin.
+  });
+
+const PORT = 3003; // Alustetaan muuttuja "PORT", joka on yhtä kuin kyseinen arvo.
+
+// Luodaan serveri portille "3003" ja tulostetaan {...} sisällä oleva teksti terminaaliin näkyviin.
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
