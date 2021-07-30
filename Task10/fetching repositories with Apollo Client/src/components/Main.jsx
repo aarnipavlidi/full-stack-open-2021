f5ebe3c3956b36df9f886e51e14348e49098c4a7
aarnipavlidi <55801258+aarnipavlidi@users.noreply.github.com>
2021-07-30 21:11:43 +0300

    Exercise 10.11: fetching repositories with Apollo Client

---
+++ Task10/fetching repositories with Apollo Client/src/components/Main.jsx
@@ -0,0 +1,45 @@
+// This exercise has been commented by Aarni Pavlidi, if you have any questions or suggestions with the code,
+// then please contact me by sending email at me@aarnipavlidi.fi <3
+
+import React from 'react'; // Otetaan käyttöön "react" niminen kirjasto sovelluksen käytettäväksi.
+import { Route, Switch, Redirect } from 'react-router-native'; // Otetaan kyseiset komponentit käyttöön "react-router-native" kirjaston kautta sovelluksen käytettäväksi.
+import Constants from 'expo-constants'; // Otetaan käyttöön "Constants" komponentti => "expo-constants" kirjaston kautta sovelluksen käytettäväksi.
+import { Text, StyleSheet, View } from 'react-native'; // Otetaan käyttöön kyseiset komponentit "react-native" kirjaston kautta sovelluksen käytettäväksi.
+
+import RepositoryList from './RepositoryList'; // Tuodaan "RepositoryList" (RepositoryList.jsx) niminen komponentti sovelluksen käytettäväksi.
+import AppBar from './AppBar'; // Tuodaan "AppBar" (AppBar.jsx) niminen komponentti sovelluksen käytettäväksi.
+import SignIn from './SignIn'; // Tuodaaan "SignIn" (SignIn.jsx) niminen komponentti sovelluksen käytettäväksi.
+
+// Alustetaan "styles" niminen muuttuja, joka suorittaa kyseisen funktion,
+// jonka kautta se saa käyttöönsä {...} sisällä olevat tyylien arvot.
+const styles = StyleSheet.create({
+  appBackground: {
+    backgroundColor: '#e1e4e8',
+    flexGrow: 1,
+    flexShrink: 1
+  },
+});
+
+// Alustetaan "Main" niminen komponentti, joka suorittaa {...} sisällä olevat asiat
+// aina, kun kyseiseen komponenttiin tehdään viittaus.
+const Main = () => {
+
+  // Komponentti renderöi (...) sisällä olevat asiat takaisin käyttäjälle näkyviin.
+  return (
+    <View style={styles.appBackground}>
+      <AppBar />
+      <Switch>
+        <Route path="/login" exact>
+          <SignIn />
+        </Route>
+        <Route path="/" exact>
+          <RepositoryList />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </View>
+  );
+};
+
+// Viedään (export) alla oleva komponentti (Main) sovelluksen käytettäväksi, jotta esim. "App.js" tiedosto pystyy suorittamaan kyseiset funktiot.
+export default Main;
