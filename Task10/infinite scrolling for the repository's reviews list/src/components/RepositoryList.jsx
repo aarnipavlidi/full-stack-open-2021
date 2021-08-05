db4ed5ae45d921f6f7215f25e05bb33f748dbd8e
aarnipavlidi <55801258+aarnipavlidi@users.noreply.github.com>
2021-08-05 19:26:33 +0300

    Exercise 10.25: infinite scrolling for the repository's reviews list

---
+++ Task10/infinite scrolling for the repository's reviews list/src/components/RepositoryList.jsx
@@ -0,0 +1,112 @@
+// This exercise has been commented by Aarni Pavlidi, if you have any questions or suggestions with the code,
+// then please contact me by sending email at me@aarnipavlidi.fi <3
+
+import React, { useState } from 'react'; // Otetaan käyttöön "react" niminen kirjasto sovelluksen käytettäväksi.
+import { useDebounce } from 'use-debounce'; // Alustetaan "useDebounce" funktio, joka hyödyntää "use-debounce" kirjaston sisältöä sovelluksen aikana.
+import { FlatList, View, StyleSheet } from 'react-native'; // Otetaan käyttöön kyseiset komponentit "react-native" kirjaston kautta sovelluksen käytettäväksi.
+
+import RepositoryItem from './RepositoryItem'; // Tuodaan "RepositoryItem" (RepositoryItem.jsx) niminen komponentti sovelluksen käytettäväksi.
+import FilterRepositoriesHeader from './FilterRepositoriesHeader'; // Tuodaan "FilterRepositoriesHeader" (FilterRepositoriesHeader.jsx) niminen komponentti sovelluksen käytettäväksi.
+import useRepositories from '../hooks/useRepositories'; // Alustetaan "useRepositories" niminen muuttuja, joka hyödyntää "useRepositories.js" tiedoston sisältöä.
+
+// Alustetaan "styles" niminen muuttuja, joka suorittaa kyseisen funktion eli
+// kun data renderöidään takaisin käyttäjälle, niin jokaisen arvon väliin
+// tulee 20px tyhjää tilaa.
+const styles = StyleSheet.create({
+  separator: {
+    height: 10,
+  },
+});
+
+// Alustetaan "RepositoryList" niminen komponentti, joka suorittaa {...} sisällä olevat asiat
+// aina, kun kyseiseen komponenttiin tehdään viittaus.
+const RepositoryList = () => {
+
+  const [currentFilter, setCurrentFilter] = useState('Latest repositories'); // Alustetaan "currentFilter" muuttuja tilaan, joka saa oletuksena kyseisen tekstin arvoksi.
+
+  const [orderByState, setOrderByState] = useState('CREATED_AT'); // Alustetaan "orderByState" muuttuja tilaan, joka saa oletuksena kyseisen tekstin arvoksi.
+  const [orderDirectionState, setOrderDirectionState] = useState('DESC'); // Alustetaan "orderDirectionState" muuttuja tilaan, joka saa oletuksena kyseisen tekstin arvoksi.
+
+  const [currentFilterInput, setCurrentFilterInput] = useState(''); // Alustetaan "currentFilterInput" muuttuja tilaan, joka saa oletuksena arvon => "".
+  // Alustetaan "debouncedFilterInput" muuttuja, joka suorittaa kyseisen funktion eli
+  // annamme "useDebounce(...)" funktiolle parametrin arvoksi "currentFilterInput".
+  // Kun käyttäjä kirjoittaa jotain hakukenttään, niin sen hetkinen arvo siirtyy
+  // "currentFilterInput" muuttujan alle, koska muutamme sen tilaa "setCurrentFilterInput"
+  // funktiota ja tämän jälkeen "debouncedFilterInput" muuttuja pääsee käsiksi
+  // kyseiseen arvoon yhden (1) sekunnin kuluttua siitä, kun "currentFilterInput"
+  // muuttuja on muuttunut sovelluksen käytön aikana. Tämän muuttujan avulla suoritetaan
+  // "useRepositories(...)" hookissa sijaitseva query ja palautetaan takaisin käyttäjälle
+  // ne arvot, jotka täsmäävät käyttäjän antaman hakukentän arvon kanssa!
+  const [debouncedFilterInput] = useDebounce(currentFilterInput, 1000);
+
+  // Alustetaan "repositories" niminen muuttuja, jonka olemme alustaneet "useRepositories"
+  // hookin kautta, missä kyseinen hook palauttaa takaisin "repositories" muuttujan datan
+  // tämän komponentin käytettäväksi. Muuttuna avulla päästään käsiksi palvelimen kautta
+  // tulevaan dataan, jotta voimme näyttää datan takaisin käyttäjälle näkyviin. Muutettu
+  // tehtävää "Exercise 10.23: sorting the reviewed repositories list" varten alla olevaa
+  // koodia niin, että sovellus suorittaa kyseisen hookin (...) sisällä annetuilla parametrin
+  // arvoilla, jonka kautta itse query eli "GET_ALL_REPOSITORIES" suoritetaan ja haetaan
+  // palvelimesta data takaisin käyttäjälle näkyviin. Aina, kun "orderByState" ja
+  // "orderDirectionState" muuttujan tilaa muutetaan, niin kyseinen hookki suorittaa
+  // queryn uudestaan ja renderöi "oikean dataan" takaisin käyttäjälle näkyviin. Lisätty
+  // "Exercise 10.24: filtering the reviewed repositories list" tehtävää varten muuttujan
+  // "debouncedFilterInput" arvo, hookin parametrin arvoksi. Kun käyttäjä saapuu sovellukseen
+  // ensimmäistä kertaa, niin query suoritetaan ensin arvolla => '', ja siitä ei tule
+  // erroria, koska query odottaa "String" tyyppiä ja kyseessä on "String" tyyppi! :)
+  //
+  // Muokattu ennen "Exercise 10.25: infinite scrolling for the repository's reviews list"
+  // tehtävää (materiaalin mukaisesti) alla olevaa hookkia niin, että kun käyttäjä
+  // saapuu sovellukseen (etusivulle), niin suoritetaan query niin että näytetään
+  // ensin viisi (5) ensimmäistä arvoa takaisin. Kun käyttäjä on päässyt "viimeisen"
+  // arvon sijaintiin niin "FlatList" komponentti renderöi lisää dataa palvelimesta
+  // "onEndReach" funktion kautta, joka on yhdistetty "onEndReach" propsin arvoon.
+  // Sekä kaikki ({...}) sisällä olevat objektit siirtyvät "variables" muuttujan
+  // parametrin arvoon (hookin tiedostossa), jonka kautta aina kun kyseinen hookki
+  // hyödyntää kyseistä parametrin arvoa, niin se viittaa aina näihin neljään (4)
+  // eri objektin arvoon.
+  const { repositories, fetchMore } = useRepositories({
+    first: 5,
+    orderBySetting: orderByState,
+    orderDirectionSetting: orderDirectionState,
+    filterInputValue: debouncedFilterInput
+  });
+
+  // Alustetaan "onEndReach" muuttuja, joka suorittaa {...} sisällä olevat asiat
+  // aina, kun kyseiseen funktioon tehdään viittaus. Funktion toiminta perustuu
+  // siihen, että aina kun "FlatList" komponentin renderöidä data näyttää "viimeisen"
+  // arvon, kun käyttäjä selaa eri arvoja käytön aikana, niin komponentti hakee
+  // lisää dataa takaisin näytettäväksi => "fetchMore(...)" funktion avulla.
+  const onEndReach = () => {
+    console.log('Fetching more repositories from server!');
+    fetchMore();
+  };
+
+  // Alustetaan "repositoryNodes" muuttuja, joka on yhtä kuin "repositories" muuttuja,
+  // jos muuttuja on "tyhjä" eli ei ole dataa, niin palautetaan takaisin "[]" eli
+  // tyhjä taulukko ja muuussa tapauksessa suoritetaan alla olevan kyseinen funktio.
+  const repositoryNodes = repositories
+    ? repositories.edges.map(results => results.node)
+    : [];
+
+  // Komponentti "RepositoryList" renderöi (...) sisällä olevat asiat takaisin käyttäjälle näkyviin.
+  // "FlatList" komponentti saa käyttöönsä alla olevat propsien arvot missä "data" määrittää mitä
+  // dataa halutaan näyttää käyttäjälle, "ItemSeparatorComponent" määrittää, että missä muodossa
+  // data näytetään (tyyli yms.) ja "renderItem" renderöi jokaisen arvon "data" propsin kautta,
+  // jotka noudattavat "ItemSeparatorComponent" komponentin rakennetta. Lisätty tehtävää varten
+  // eli "Exercise 10.11: fetching repositories with Apollo Client" => "keyExtractor" propsi, koska
+  // ilman sitä tuli "VirtualizedList: missing keys for items..." erroria terminaaliin näkyviin!
+  return (
+    <FlatList
+      data={repositoryNodes}
+      onEndReached={() => onEndReach()}
+      onEndReachedThreshold={0.5}
+      ItemSeparatorComponent={() => <View style={styles.separator} />}
+      keyExtractor={(item, index) => item.fullName}
+      renderItem={({ item }) => <RepositoryItem item={item} />}
+      ListHeaderComponent={<FilterRepositoriesHeader currentFilter={currentFilter} setCurrentFilter={setCurrentFilter} currentFilterInput={currentFilterInput} setCurrentFilterInput={setCurrentFilterInput} setOrderByState={setOrderByState} setOrderDirectionState={setOrderDirectionState} />}
+    />
+  );
+};
+
+// Viedään (export) alla oleva komponentti (RepositoryList) sovelluksen käytettäväksi, jotta esim. "App.js" tiedosto pystyy suorittamaan kyseiset funktiot.
+export default RepositoryList;
---
+++ Task10/infinite scrolling for the repository's reviews list/src/graphql/queries.js
@@ -0,0 +1,139 @@
+// This exercise has been commented by Aarni Pavlidi, if you have any questions or suggestions with the code,
+// then please contact me by sending email at me@aarnipavlidi.fi <3
+
+import { gql } from '@apollo/client' // Sovellus ottaa käyttöön kyseiset funktiot "@apollo/client" kirjaston kautta.
+
+// Alustetaan "GET_ALL_REPOSITORIES" niminen query, joka suorittaa alla olevan queryn
+// aina, kun kyseiseen queryyn tehdään viittaus. Query palauttaa takaisin palvelimen
+// kautta tulevan datan, josta löytyy jokainen "repositories" arvo.
+//
+// Muutettu alla olevaa queryä "GET_ALL_REPOSITORIES" tehtävää "Exercise 10.23: sorting the reviewed repositories list"
+// varten niin, että sovellus suorittaa aina queryn kahdella (2) eri parametrin avulla eli "orderBySetting" sekä
+// "orderDirectionSetting". Ensimmäinen vaihtoehto noudattaa "AllRepositoriesOrderBy" tyyppiä, missä vaihtoehtona
+// on joko "CREATED_AT" tai "RATING_AVERAGE" sekä toinen vaihtoehto noudattaa "OrderDirection" tyyppiä missä
+// vaihtoehtona on joko "DESC" tai "ASC". Oletuksena kun käyttäjä avaa sovelluksen ensimmäistä kertaa tai palaa
+// takaisin näkymään missä on kaikki "repository":n arvot, niin query saa parametrin arvoksi "CREATED_AT" ja
+// "DESC" eli näytetään käyttäjälle aina ensimmäisenä viimeisin/uusin arvostelu siihen "repository":n arvoon.
+//
+// Lisätty "Exercise 10.24: filtering the reviewed repositories list" tehtävää varten uusi parametin arvo eli
+// "$filterInputValue", ja query odottaa että kyseessä on "String" tyyppi. Muuttujan arvo sijoitetaan objektin
+// "searchKeyword" alle, jonka kautta queryn suorittamisen yhteydessä palautetaan ne arvot mitkä täsmäävät
+// käyttäjän antaman hakukentän arvon kanssa. Oletuksena objektin arvo saa => '' eli se palauttaa kaikki
+// "repository":n arvot takaisin käyttäjälle näkyviin sovelluksen etusivulle.
+//
+// Lisätty ""Exercise 10.25: infinite scrolling for the repository's reviews list" tehtävää
+// varten "first" ja "after" argumenttien käyttäminen queryn osalta. Näiden toiminnallisuus
+// selitetty tarkemmin "GET_CURRENT_REPOSITORY_REVIEWS" queryn osalta.
+export const GET_ALL_REPOSITORIES = gql`
+  query showAllRepositories($first: Int, $after: String, $orderBySetting: AllRepositoriesOrderBy, $orderDirectionSetting: OrderDirection, $filterInputValue: String) {
+    repositories(first: $first, after: $after, orderBy: $orderBySetting, orderDirection: $orderDirectionSetting, searchKeyword: $filterInputValue) {
+      edges {
+        node {
+          id
+          fullName
+          description
+          language
+          stargazersCount
+          forksCount
+          reviewCount
+          ratingAverage
+          ownerAvatarUrl
+        }
+        cursor
+      }
+      pageInfo {
+        endCursor
+        startCursor
+        hasNextPage
+      }
+    }
+  }
+`
+
+// Alustetaan "GET_CURRENT_REPOSITORY" niminen query, joka suorittaa alla olevan
+// queryn aina, kun kyseiseen queryyn tehdään viittaus. Queryn toiminta on täysin
+// samanlainen kuin yllä oleva (GET_ALL_REPOSITORIES), mutta sen sijaan että me
+// haetaan kaikki arvot palvelimesta, niin me haetaan vain yksi "repositoryID"
+// objektin avulla, joka siirretään => "id" objektin argumentiksi.
+export const GET_CURRENT_REPOSITORY = gql`
+  query showCurrentRepository($repositoryID: ID!) {
+    repository(id: $repositoryID) {
+      id
+      fullName
+      url
+      description
+      language
+      stargazersCount
+      forksCount
+      reviewCount
+      ratingAverage
+      ownerAvatarUrl
+    }
+  }
+`
+
+// Alustetaan "GET_CURRENT_REPOSITORY_REVIEWS" niminen query, joka suorittaa
+// alla olevan queryn aina, kun kyseiseen queryyn tehdään viittaus. Query
+// näyttää siis sen hetkisen klikatun "repository" arvon kaikki arvostelut.
+// Jos sen hetkisellä arvolla ei ole annettu toistaiseksi mitään arvostelua,
+// niin sovellus renderöi takaisin tyhjän taulukon. Tämän ominaisuus on
+// toteutettu "RepositoryListByID" komponentissa.
+//
+// Muutettu "Exercise 10.25: infinite scrolling for the repository's reviews list"
+// tehtävää varten alla olevaa queryä niin, että sille lisätty "first" ja "after"
+// argumenttien käytön mahdollisuus. Objektin arvo "first" avulla voidaan määrittää,
+// että kuinka monta arvoa halutaan näyttää takaisin käyttäjälle (palvelimen kautta
+// tulevasta datasta) ja, kun sovellus haluaa näyttää lisää arvoja eli käyttäjä on
+// päässyt viimeisen arvon kohdalle, niin objektin "after" avulla voidaan näyttää
+// seuravaat arvot palvelimesta, jos niitä löytyy palvelimesta. Käytännössä tämä
+// tarkoittaa sitä, että jos me halutaan esim. renderöidä kaksi (2) ensimmäistä
+// arvoa niin => "pageInfo.startCursor" kertoo meille, että mikä on ensimmäisen
+// arvon "cursor" objektin arvo ja "pageInfo.endCursor" kertoo meille että mikä
+// on toisen "cursor" objektin arvo. (jokaiselta arvolta löytyy siis "cursor"
+// objektin arvo). Tästä me voidaan päätellä, että jos palvelimesta vielä löytyy
+// lisää näiden kahden (2) arvon jälkeen, niin "hasNextPage" on arvoa => "true",
+// jonka jälkeen me sijoitetaan "after" objektin arvoon => "pageInfo.endCursor",
+// tästä sovellus tietää, että mitä arvoa käyttäjälle on näytetty viimeisenä, jonka
+// kautta varmistetaan että ei renderöidä "samanlaisia arvoja" takaisin.
+export const GET_CURRENT_REPOSITORY_REVIEWS = gql`
+  query showCurrentRepositoryReviews($repositoryID: ID!, $first: Int, $after: String) {
+    repository(id: $repositoryID) {
+      id
+      fullName
+      reviews(first: $first, after: $after) {
+        totalCount
+        edges {
+          node {
+            id
+            text
+            rating
+            createdAt
+            repositoryId
+            user {
+              id
+              username
+            }
+          }
+          cursor
+        }
+        pageInfo {
+          endCursor
+          startCursor
+          hasNextPage
+        }
+      }
+    }
+  }
+`
+
+// Alustetaan "GET_CURRENT_USER_DATA" niminen query, joka suorittaa alla olevan queryn
+// aina, kun kyseiseen queryyn tehdään viittaus. Query palauttaa takaisin palvelimen
+// kautta tulevan data, josta löytyy sen hetkinen kirjatunut käyttäjä.
+export const GET_CURRENT_USER_DATA = gql`
+  query {
+    authorizedUser {
+      id
+      username
+    }
+  }
+`
