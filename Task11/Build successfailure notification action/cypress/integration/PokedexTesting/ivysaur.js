0da4b97ac9245e5f66ee734916880bfa1c5369a9
aarnipavlidi <55801258+aarnipavlidi@users.noreply.github.com>
2021-08-10 00:04:31 +0300

    11.19 Build success/failure notification action

---
+++ Task11/Build successfailure notification action/cypress/integration/PokedexTesting/ivysaur.js
@@ -0,0 +1,11 @@
+describe('Pokedex', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+  it('Pokemon called Charmander can be opened and has right content', function() {
+    cy.get('#app').get('div').should('have.class', 'list-container').find('a').should('have.class', 'list-item').contains('charmander').click()
+    cy.get('#app').get('div').should('have.class', 'pokemon-name').contains('charmander').get('div').should('have.class', 'pokemon-ability-type').contains('Hidden ability')
+  })
+})
